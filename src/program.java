2671c98a61ad09469ed859e948ccda68f4b81b08
willian.albeche <willian.albeche@edu.pucrs.br>
2021-04-20 22:59:25 -0300

    Changes WOW
    Vitor cansou

--- src/program.java
+++ src/program.java
@@ -6,7 +6,6 @@ import readers.FileModifier;
 public class program {
         public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Vm interpreter = new Vm();
         int opcao = 0;
         String logo = "______               _        ______               _     \n";
         logo += "| ___ \\             (_)       |  ___|             | |    \n";
@@ -15,24 +14,23 @@ public class program {
         logo += "| |_/ /| |   | (_| || || | | || |    | |_| || (__ |   <  \n";
         logo += "\\____/ |_|    \\__,_||_||_| |_|\\_|     \\__,_| \\___||_|\\_\\ \n";
         logo += "\n";
-
+        
         
         do {
             System.out.print("\033[H\033[2J");
             System.out.print("\033[H\033[2J");
+            Vm interpreter = new Vm();
 
             System.out.println(logo);
 
-
+            System.out.println("Para executar varios programas, basta atualizar o SOURCE.txt e o IF.txt ;) ");
             System.out.println("|=====================================|");
             System.out.println("|                MENU                 |");
             System.out.println("|=====================================|");
             System.out.println("|                                     |");
             System.out.println("|  [0] - Sair                         |");
-            System.out.println("|  [1] - Inserir Input                |");
-            System.out.println("|  [2] - Inserir programa             |");
-            System.out.println("|  [3] - Executar Programa            |");
-            System.out.println("|  [4] - Mostrar saida                |");
+            System.out.println("|  [1] - Executar Programa            |");
+            System.out.println("|  [2] - Mostrar saida                |");
             System.out.println("|                                     |");
             System.out.println("|=====================================|");
             
@@ -45,24 +43,12 @@ public class program {
                     System.exit(0);
                     break;
                 case 1:
-                    System.out.println("Input:");
-                    String input = in.next();
-                    FileModifier.clean("IF.txt");
-                    FileModifier.write("IF.txt", input);
-                    break;
-                case 2: 
-                    System.out.println("Escreva um programa: ");
-                    input = in.next();
-                    FileModifier.clean("SOURCE.txt");
-                    FileModifier.write("SOURCE.txt", input);
-                    break;
-                case 3:
                     System.out.println("Executando...\n\n");
                     System.out.println("Programa: ");
                     System.out.println(FileModifier.read("SOURCE.txt"));
                     interpreter.runProgram();
                     break;
-                case 4:
+                case 2:
                     System.out.println("Output:");
                     System.out.println(FileModifier.read("OF.txt"));
                     break;
